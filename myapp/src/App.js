dbef48396890c08837b54eb0a1409e850a05c96d
Akash Dixit <83697333+a-Cash-dixit@users.noreply.github.com>
2023-12-03 20:28:58 +0530

    Deployment left

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import { getUsers } from "./UserService";
 import User from "./components/UserComponent";
 import React from "react";
+import { useMemo } from "react";
+import Pagination from "./Pagination";
+let PageSize = 10;
+
 function App() {
   const [users, setUsers] = React.useState([]);
   const [update, setUpdate] = React.useState(false);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  
   React.useEffect(() => {
     getUsers(setUsers);
   }, []);
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return users.slice(firstPageIndex, lastPageIndex);
+  }, [users,currentPage]);
   const deleteUser = (id) => {
     let tempUsers = users.filter((user) => user.id !== id);
     setUsers(tempUsers);
@@ -35,23 +46,25 @@ function App() {
     });
   };
   const searchUsers = (e) => {
-    //setPage(1);
+    setCurrentPage(1);
     setUsers(searchInUsers(e.target.value, users));
   };
   const select = (id) => {
     let tempUsers = users;
-    const index = tempUsers.findIndex((user) => user.id === id);
+    let index = tempUsers.findIndex((user) => user.id === id);
     tempUsers[index].selected = !tempUsers[index].selected;
     setUsers(tempUsers);
     setUpdate((prevState) => !prevState);
   };
   const selectAll = (e) => {
-    const listedUserIds = users
+    const index=(currentPage-1)*10;
+    const UserIds = users
       .filter((user) => user.show)
+      .slice(index, index + 10)
       .map((user) => user.id);
 
     let tempUsers = users.map((user) => {
-      if (listedUserIds.includes(user.id)) {
+      if (UserIds.includes(user.id)) {
         user.selected = e.target.checked;
         return user;
       }
@@ -62,10 +75,17 @@ function App() {
     setUpdate(!update);
   };
   const deleteSelected = () => {
-    if (window.confirm("Selected users will be deleted")) {
-      setUsers((prevState) => prevState.filter((user) => !user.selected));
-      //selectAllRef.current.checked = false;
-    }
+    setUsers((prevState) => prevState.filter((user) => !user.selected));
+  };
+  const saveUser = (id, nameRef, emailRef, roleRef) => {
+    let tempUsers = users;
+    const index = tempUsers.findIndex((user) => user.id === id);
+    tempUsers[index].name = nameRef.current.value;
+     tempUsers[index].email = emailRef.current.value;
+     tempUsers[index].role = roleRef.current.value;
+    tempUsers[index].edit = false;
+    setUsers(tempUsers);
+    setUpdate((prevState) => !prevState);
   };
   return (
     <>
@@ -77,6 +97,7 @@ function App() {
           gap: "5px",
           width: "80%",
           margin: "auto",
+          padding:"2rem"
         }}
       >
         <input
@@ -103,7 +124,7 @@ function App() {
             Role
           </div>
         </div>
-        {users.map((user) => {
+        {currentTableData.map((user) => {
           return user.show ? (
             <User
               editUser={editUser}
@@ -112,15 +133,23 @@ function App() {
               deleteSelected={deleteSelected}
               key={user.id}
               user={user}
+              saveUser={saveUser}
             ></User>
           ) : (
             ""
           );
         })}
       </div>
-      <button style={{ marginLeft: "150px" }} onClick={() => deleteSelected()}>
+      <button style={{ marginLeft: "150px",background:"red" }} onClick={() => deleteSelected()}>
         Delete Selected
       </button>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={users.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
     </>
   );
 }
--- myapp/src/components/UserComponent.js
+++ myapp/src/components/UserComponent.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { useRef } from "react";
 const User = (props) => {
   //console.log(user.user.id);
-  const { user, deleteUser, editUser,select } = props;
-
+  const { user, deleteUser, editUser,select,saveUser } = props;
+  const nameRef = useRef(null);
+  const emailRef=useRef(null);
+  const roleRef=useRef(null);
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
         border: "1px grey solid",
+        background:`${user.selected? "grey":"white"}`
       }}
     >
       <input
@@ -18,13 +22,43 @@ const User = (props) => {
         onChange={() => select(user.id)}
         checked={user.selected}
       ></input>
-      <div style={{ margin: "auto" }}>{user.name}</div>
-      <div style={{ margin: "auto" }}>{user.email}</div>
-      <div style={{ margin: "auto" }}>
-        {user.role}
-        <i onClick={() => editUser(user.id)} className="fas fa-edit"></i>
-        <i onClick={() => deleteUser(user.id)} className="fas fa-trash-alt"></i>
+      <div style={{ margin: "auto" }}><input
+      style={{border:"none",background:"transparent"}}
+          readOnly={!user.edit}
+          type="text"
+          name="name"
+          ref={nameRef}
+          defaultValue={user.name}
+        ></input></div>
+      
+      <div style={{ margin: "auto" }}><input
+      style={{border:"none",background:"transparent"}}
+          readOnly={!user.edit}
+          type="text"
+          name="email"
+          ref={emailRef}
+          defaultValue={user.email}
+        ></input></div>
+      
+      <div style={{ margin: "auto",display:"flex",justifyContent:"flex-end" }}>
+      <input
+      style={{border:"none",background:"transparent"}}
+          readOnly={!user.edit}
+          type="text"
+          name="role"
+          ref={roleRef}
+          defaultValue={user.role}
+        ></input>
+        
       </div>
+      <div style={{display:"flex"}}>
+      {user.edit? <i
+      style={{margin:"auto"}} 
+            onClick={() => saveUser(user.id, nameRef, emailRef, roleRef)}
+            className="fas fa-save"
+          ></i>: <i style={{margin:"auto"}}  onClick={() => editUser(user.id)} className="fas fa-edit"></i>}
+        <i style={{margin:"auto"}} onClick={() => deleteUser(user.id)} className="fas fa-trash-alt"></i>
+        </div>
     </div>
   );
 };
